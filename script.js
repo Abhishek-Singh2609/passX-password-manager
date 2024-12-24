a26e83d730b3fa54929969b0ab67d28f8e6bca61
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2024-12-25 00:12:38 +0530

    Add SVG icon for clipboard and implement password masking and copying functionality

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function maskPassword(pass) {
+  let str = "";
+  for (let index = 0; index < pass.length; index++) {
+    str += "*";
+  }
+  return str;
+}
+
+function copyText(txt) {
+  navigator.clipboard.writeText(txt).then(
+    () => {
+      /* clipboard successfully set */
+      document.getElementById("alert").style.display = "inline";
+      setTimeout(() => {
+        document.getElementById("alert").style.display = "none";
+      }, 2000);
+    },
+    () => {
+      /* clipboard write failed */
+      alert("Clipboard copying failed");
+    }
+  );
+}
+
+const deletePassword = (website) => {
+  let data = localStorage.getItem("passwords");
+  let arr = JSON.parse(data);
+  arrUpdated = arr.filter((e) => {
+    return e.website != website;
+  });
+  localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+  alert(`Successfully deleted ${website}'s password`);
+  showPasswords();
+};
+
 // Logic to fill the table
 const showPasswords = () => {
   let tb = document.querySelector("table");
